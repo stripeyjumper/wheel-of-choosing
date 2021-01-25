749cfc9b1f44b4965e477b9829e7d54147ae784b
Andrew Bain <andrew.bain@sage.com>
2021-01-25 08:50:11 +0000

    feat(names): add name list

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
-import React from 'react';
-import './App.css';
-import NameList from './components/NameList';
-import Wheel from './components/Wheel'
+import React, { useCallback, useMemo, useState } from "react";
+import "./App.css";
+import NameList from "./components/NameList";
+import Wheel from "./components/Wheel";
 
 function App() {
+  const [{ segments }, setState] = useState({
+    segments: [
+      { id: "1", label: "Toot", color: "#f00" },
+      { id: "2", label: "Toot", color: "#ff0" },
+      { id: "3", label: "Toot", color: "#f0f" },
+    ],
+  });
+  const [isSpinning, setIsSpinning] = useState(false);
+
+  const handleSpin = useCallback(() => {
+    setIsSpinning((prevIsSpinning) => !prevIsSpinning);
+  }, []);
+
+  const names = useMemo(() => segments.map(({ label }) => label), [segments]);
+
+  const handleChanged = useCallback((names: string[]) => {
+    setState((prevState) => ({
+      ...prevState,
+      segments: names.map((name, i) => ({
+        id: `${i}`,
+        label: name,
+        color: "#0ff",
+      })),
+    }));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Wheel of choosings</h1>
+        <h1>Wheel of choosing</h1>
       </header>
-      <NameList/>
-      <Wheel/>
+      <NameList names={names} onChanged={handleChanged} />
+      <Wheel segments={segments} onSpin={handleSpin} isSpinning={isSpinning} />
     </div>
   );
 }
--- src/components/NameList.tsx
+++ src/components/NameList.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useCallback } from "react";
 
-function NameList(){
-  return <p>Name list</p>
+interface NameListProps {
+  names: string[];
+  onChanged: (names: string[]) => void;
+}
+
+function NameList({ names, onChanged }: NameListProps) {
+  return (
+    <>
+      {names.map((name, i) => {
+        return (
+          <NameInput
+            name={name}
+            onNameChanged={(value) => {
+              const nextNames = [...names];
+              nextNames[i] = value;
+              onChanged(nextNames);
+            }}
+            onDelete={() => null}
+          />
+        );
+      })}
+    </>
+  );
+}
+
+interface NameInputProps {
+  name: string;
+  onNameChanged: (value: string) => void;
+  onDelete: () => void;
+}
+
+function NameInput({ name, onNameChanged, onDelete }: NameInputProps) {
+  const handleChange = useCallback(
+    (e) => {
+      onNameChanged(e.target.value);
+    },
+    [onNameChanged]
+  );
+  return <input type="text" value={name} onChange={handleChange} />;
 }
 
 export default NameList;
