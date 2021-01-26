cfeaf91f3832191562c2ab681d67d8878350c453
Andrew Bain <andrew.bain@sage.com>
2021-01-26 08:40:48 +0000

    feat(colors): wheel segment colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React, { useCallback, useMemo, useState } from "react";
 import "./App.css";
 import NameList from "./components/NameList";
 import Wheel from "./components/Wheel";
+import { getColors } from "./get-colors";
+
+const baseColors = ["#488f31", "#ffe48f", "#de425b"];
 
 function App() {
   const [{ segments }, setState] = useState({
     segments: [
-      { id: "1", label: "Toot", color: "#f00" },
-      { id: "2", label: "Toot", color: "#ff0" },
-      { id: "3", label: "Toot", color: "#f0f" },
+      { id: "1", label: "Toot" },
+      { id: "2", label: "Toot" },
+      { id: "3", label: "Toot" },
+      { id: "4", label: "Toot" },
+      { id: "5", label: "Toot" },
+      { id: "6", label: "Toot" },
+      { id: "7", label: "Toot" },
     ],
   });
   const [isSpinning, setIsSpinning] = useState(false);
@@ -19,16 +26,25 @@ function App() {
 
   const names = useMemo(() => segments.map(({ label }) => label), [segments]);
 
-  const handleChanged = useCallback((names: string[]) => {
-    setState((prevState) => ({
-      ...prevState,
-      segments: names.map((name, i) => ({
-        id: `${i}`,
-        label: name,
-        color: "#0ff",
+  const handleChanged = useCallback(
+    (names: string[]) =>
+      setState((prevState) => ({
+        ...prevState,
+        segments: names.map((name, i) => ({
+          id: `${i}`,
+          label: name,
+        })),
       })),
+    []
+  );
+
+  const segmentsWithColors = useMemo(() => {
+    const colors = getColors(segments.length, baseColors);
+    return segments.map((segment, i) => ({
+      ...segment,
+      color: colors[i],
     }));
-  }, []);
+  }, [segments]);
 
   return (
     <div className="App">
@@ -36,7 +52,11 @@ function App() {
         <h1>Wheel of choosing</h1>
       </header>
       <NameList names={names} onChanged={handleChanged} />
-      <Wheel segments={segments} onSpin={handleSpin} isSpinning={isSpinning} />
+      <Wheel
+        segments={segmentsWithColors}
+        onSpin={handleSpin}
+        isSpinning={isSpinning}
+      />
     </div>
   );
 }
---
+++ src/get-colors.ts
@@ -0,0 +1,35 @@
+import Color from "color";
+
+export function getColors(count: number, baseColors: string[]) {
+  if (baseColors.length < 2) {
+    throw new Error("At least two base colors are required");
+  }
+
+  if (count <= 1) {
+    return [Color(baseColors[0]).hex()];
+  }
+
+  const colors = baseColors.map((c) => new Color(c));
+
+  const results = [];
+  for (let i = 0; i < count; i += 1) {
+    const angle = ((2 * Math.PI) / count) * i;
+    const color = getColor(angle, colors);
+    results.push(color.hex().toString());
+  }
+
+  return results;
+}
+
+function getColor(angle: number, colors: Color[]) {
+  const anglePerColor = (2 * Math.PI) / colors.length;
+  const startIndex = Math.floor(angle / anglePerColor);
+  const startColor = colors[startIndex];
+
+  const endIndex = (startIndex + 1) % colors.length;
+  const endColor = colors[endIndex];
+
+  const weight = (angle % anglePerColor) / anglePerColor;
+
+  return startColor.mix(endColor, weight);
+}
