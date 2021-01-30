7484a9d2fbdbcb625c08d44c37b765f3dfaed8a8
Andrew Bain <12202813+stripeyjumper@users.noreply.github.com>
2021-01-30 11:56:59 +0000

    feat(list): list improvements

--- src/components/NameList.tsx
+++ src/components/NameList.tsx
@@ -1,27 +1,37 @@
-import React, { useCallback } from "react";
-
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
 interface NameListProps {
   names: string[];
   onChanged: (names: string[]) => void;
 }
 
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 function NameList({ names, onChanged }: NameListProps) {
   return (
-    <>
+    <ListContainer>
       {names.map((name, i) => {
         return (
           <NameInput
+            key={i}
             name={name}
             onNameChanged={(value) => {
               const nextNames = [...names];
               nextNames[i] = value;
               onChanged(nextNames);
             }}
-            onDelete={() => null}
+            onDelete={() => {
+              const nextNames = [...names];
+              nextNames.splice(i, 1);
+              onChanged(nextNames);
+            }}
           />
         );
       })}
-    </>
+    </ListContainer>
   );
 }
 
@@ -31,14 +41,79 @@ interface NameInputProps {
   onDelete: () => void;
 }
 
+const InputRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 200px;
+`;
+
+const ReadOnlyInput = styled.p`
+  margin: 0;
+  padding: 0.2em;
+  font-size: 10pt;
+  font-family: sans-serif;
+  border: 1px solid transparent;
+`;
+
+const StyledInput = styled.input`
+  margin: 0;
+  padding: 0.2em;
+  font-size: 10pt;
+  font-family: sans-serif;
+  border: 1px solid transparent;
+`;
+
+const DeleteButton = styled.button``;
+
 function NameInput({ name, onNameChanged, onDelete }: NameInputProps) {
+  const inputRef = useRef<any>();
+  const [hasFocus, setHasFocus] = useState(false);
+  const [hasMouseDown, setHasMouseDown] = useState(false);
+
   const handleChange = useCallback(
     (e) => {
       onNameChanged(e.target.value);
     },
     [onNameChanged]
   );
-  return <input type="text" value={name} onChange={handleChange} />;
+
+  const handleBlur = useCallback(() => !hasMouseDown && setHasFocus(false), [
+    hasMouseDown,
+  ]);
+  const handleFocus = useCallback(() => setHasFocus(true), []);
+
+  useEffect(() => {
+    if (hasFocus && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [hasFocus]);
+
+  return (
+    <InputRow onBlur={handleBlur} onClick={handleFocus}>
+      {hasFocus ? (
+        <>
+          <StyledInput
+            ref={inputRef}
+            type="text"
+            value={name}
+            onChange={handleChange}
+          />
+          <DeleteButton
+            type="button"
+            onTouchStart={() => setHasMouseDown(true)}
+            onMouseDown={() => setHasMouseDown(true)}
+            onTouchEnd={() => setHasMouseDown(false)}
+            onMouseUp={() => setHasMouseDown(false)}
+            onClick={onDelete}
+          >
+            Delete
+          </DeleteButton>
+        </>
+      ) : (
+        <ReadOnlyInput>{name}</ReadOnlyInput>
+      )}
+    </InputRow>
+  );
 }
 
 export default NameList;
--- src/components/Wheel.tsx
+++ src/components/Wheel.tsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef } from "react";
+import styled from "styled-components";
 import WheelSegment from "./WheelSegment";
 
 interface WheelProps {
   segments: { id: string; label: string; color: string }[];
   onSpin: () => void;
-  isSpinning: boolean;
+  selectedSegmentId?: string;
 }
 
 const defaultProps = {
   radius: 90,
 };
 
-function Wheel({ segments, onSpin, isSpinning }: WheelProps) {
+const WheelGroup = styled.g<any>`
+  transform-origin: 50% 50%;
+  @keyframes wheel--animation {
+    0% {
+      transform: rotate(0deg) translate(100px, 100px);
+    }
+
+    100% {
+      transform: rotate(${({ spinAngle }) => `${spinAngle}`}deg)
+        translate(100px, 100px);
+    }
+  }
+
+  animation-duration: ${({ spinDuration }) => `${spinDuration}`}s;
+  animation-timing-function: ease-out;
+  animation-iteration-count: 1;
+  animation-fill-mode: forwards;
+  animation-name: wheel--animation;
+`;
+
+function usePrevious<TValue>(value: TValue) {
+  const ref = useRef<TValue>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+function Wheel({ segments, onSpin, selectedSegmentId }: WheelProps) {
   const { radius } = defaultProps;
 
   const angle = (2 * Math.PI) / segments.length;
+
+  const prevSelectedSegmentId = usePrevious(selectedSegmentId);
+  const { spinAngle, spinDuration } = useMemo(() => {
+    return { spinAngle: 720, spinDuration: 3 };
+  }, [selectedSegmentId, prevSelectedSegmentId]);
 
   return (
     <svg
@@ -23,7 +57,12 @@ function Wheel({ segments, onSpin, isSpinning }: WheelProps) {
       style={{ position: "relative", pointerEvents: "none" }}
       viewBox="0 0 200 200"
     >
-      <g transform="translate(100,100)" stroke="#000">
+      <WheelGroup
+        stroke="#000"
+        id="wheel"
+        spinAngle={spinAngle}
+        spinDuration={spinDuration}
+      >
         {segments.map(({ id, label, color }, i) => {
           const rotation = i * angle;
 
@@ -40,7 +79,7 @@ function Wheel({ segments, onSpin, isSpinning }: WheelProps) {
             />
           );
         })}
-      </g>
+      </WheelGroup>
     </svg>
   );
 }
