f0656b395df37428fff02e55c8e596121e39ec3f
Andrew Bain <12202813+stripeyjumper@users.noreply.github.com>
2021-02-03 22:51:39 +0000

    feat(wheel): add framer motion

--- src/components/Wheel.tsx
+++ src/components/Wheel.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
-import styled from "styled-components";
 import WheelSegment from "./WheelSegment";
 import { usePreviousValue } from "./use-previous-value";
+import { motion } from "framer-motion";
 
 interface WheelProps {
   segments: { id: string; label: string; color: string }[];
@@ -13,50 +13,27 @@ const defaultProps = {
   radius: 90,
 };
 
-const WheelGroup = styled.g<any>`
-  transform-origin: 50% 50%;
-  @keyframes wheel--animation {
-    0% {
-      transform: rotate(0deg) translate(100px, 100px);
-    }
-
-    100% {
-      transform: rotate(${({ spinAngle }) => `${spinAngle}`}deg)
-        translate(100px, 100px);
-    }
-  }
-
-  animation-duration: ${({ spinDuration }) => `${spinDuration}`}s;
-  animation-timing-function: ease-out;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-name: wheel--animation;
-`;
+const WheelGroup = motion.g;
 
 function Wheel({ segments, onSpin, selectedSegmentId }: WheelProps) {
   const { radius } = defaultProps;
 
   const angle = (2 * Math.PI) / segments.length;
 
-  const prevSelectedSegmentId = usePreviousValue(selectedSegmentId);
+  // const prevSelectedSegmentId = usePreviousValue(selectedSegmentId);
 
-  const { spinAngle, spinDuration } = useMemo(() => {
-    return { spinAngle: 720, spinDuration: 3 };
-  }, [selectedSegmentId, prevSelectedSegmentId]);
+  // const { spinAngle, spinDuration } = useMemo(() => {
+  //   return { spinAngle: 720, spinDuration: 3 };
+  // }, [selectedSegmentId, prevSelectedSegmentId]);
 
   return (
     <svg
       width="600"
-      height="600"
+      height="400"
       style={{ position: "relative", pointerEvents: "none" }}
       viewBox="0 0 200 200"
     >
-      <WheelGroup
-        stroke="#000"
-        id="wheel"
-        spinAngle={spinAngle}
-        spinDuration={spinDuration}
-      >
+      <WheelGroup stroke="#000">
         {segments.map(({ id, label, color }, i) => {
           const rotation = i * angle;
 
