8ac22cce01807abd8d17e01b56390e3fc03eec88
Andrew Bain <12202813+stripeyjumper@users.noreply.github.com>
2021-06-13 18:52:15 +0100

    remove api folder

--- api/src/validate-wheels.js
+++
@@ -1,7 +0,0 @@
-const Ajv = require("ajv").default;
-const { wheelSchema } = require("./wheel-schema");
-
-const ajv = new Ajv({strict:false});
-const validateWheels = ajv.compile(wheelSchema);
-
-exports.validateWheels = validateWheels;
