f244b1212c942a53780b5e979316c765b2df2a20
Andrew Bain <andrew.bain@sage.com>
2022-06-13 09:41:36 +0100

    use random initial wheel angle again

--- src/components/WheelPanel/Wheel.tsx
+++ src/components/WheelPanel/Wheel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import WheelSegment, { EmptyWheel } from "./WheelSegment";
 import { motion, useMotionTemplate, useSpring } from "framer-motion";
 import { getRandomInteger } from "../../helpers/get-random-integer";
@@ -180,6 +180,11 @@ const CountOfNames = styled.span`
   opacity: 0.7;
 `;
 
+const useRandomAngle = () => {
+  const [value] = useState(() => Math.random() * 2 * Math.PI);
+  return value;
+};
+
 function Wheel({
   label,
   segments,
@@ -195,7 +200,9 @@ function Wheel({
   const duration = 2000;
   const isEmpty = !segments.length;
 
-  const spinAngle = useSpring(0, {
+  const randomAngle = useRandomAngle();
+
+  const spinAngle = useSpring(isEmpty ? 0 : randomAngle, {
     duration: 2000,
   });
   const segmentAngle = (2 * Math.PI) / segments.length;
